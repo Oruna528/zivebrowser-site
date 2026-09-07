8501b7e5aa58113aed9f5281bfc81661c315ed06
Oruna <haru.k6108@icloud.com>
2026-09-07 18:10:58 +0900

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,7 +1,7 @@
 import { getCloudflareContext } from '@opennextjs/cloudflare'
 import { NextResponse } from 'next/server'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
